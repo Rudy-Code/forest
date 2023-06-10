e236bfad3c128001e1830f7473661d29b115f89a
RUDYGAMES <rudygamesv2@gmail.com>
2023-06-10 22:44:07 +0200

    fixed link music test1

--- src/js/offersSpecial.js
+++ src/js/offersSpecial.js
@@ -3,7 +3,7 @@ const offersBtnSpecial = document.querySelector('.offers__card-btn--special')
 const specialCloseBtn = document.querySelector('.special__close-btn')
 
 // music file to play
-const music = new Audio('../music/fortnite.mp3')
+const music = new Audio('/music/fortnite.mp3')
 
 const open = () => {
 	music.play()
