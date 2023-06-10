8b6986301d679a903a5d90b990c1d6846629a661
RUDYGAMES <rudygamesv2@gmail.com>
2023-06-10 22:56:29 +0200

    fixed bugs offers special v3

--- src/js/offersSpecial.js
+++ src/js/offersSpecial.js
@@ -1,6 +1,7 @@
 const popupSpecial = document.querySelector('.special')
 const offersBtnSpecial = document.querySelector('.offers__card-btn--special')
 const specialCloseBtn = document.querySelector('.special__close-btn')
+const video = document.querySelector('.video')
 
 // music file to play
 const music = new Audio('/music/fortnite.mp3')
@@ -8,11 +9,12 @@ const music = new Audio('/music/fortnite.mp3')
 const open = () => {
 	music.play()
 	popupSpecial.classList.add('show-special')
+	video.play()
 }
 const close = () => {
 	music.pause()
 	popupSpecial.classList.remove('show-special')
-
+	video.pause()
 }
 
 offersBtnSpecial.addEventListener('dblclick', open)
