e8d14d074b7087bad5e7c3d06c2adbe24d0bebdd
RUDYGAMES <rudygamesv2@gmail.com>
2023-06-11 18:39:25 +0200

    changed special thanks

--- src/js/offersSpecial.js
+++ src/js/offersSpecial.js
@@ -1,22 +1,13 @@
 const popupSpecial = document.querySelector('.special')
-const offersBtnSpecial = document.querySelector('.offers__card-btn--special')
-const specialCloseBtn = document.querySelector('.special__close-btn')
-const video = document.querySelector('.video')
 
-// music file to play
+const openBtn = document.querySelector('.open-btn')
 const music = new Audio('/music/fortnite.mp3')
 
 const open = () => {
+	openBtn.style.display = 'none'
 	music.play()
 	popupSpecial.classList.add('show-special')
 	video.play()
 }
-const close = () => {
-	music.pause()
-	popupSpecial.classList.remove('show-special')
-	video.pause()
-}
 
-offersBtnSpecial.addEventListener('dblclick', open)
-
-specialCloseBtn.addEventListener('click', close)
+openBtn.addEventListener('click', open)
